16a25dc232fca2946a77f0733e390247cb5e2444
Giulia Pizzignacco <beepelucio@gmail.com>
2017-08-30 15:52:34 +0000

    create edit option to articles and created a partial form to render form on new and edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,17 @@ class ArticlesController < ApplicationController
     end
         
     def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end
     
     private 
